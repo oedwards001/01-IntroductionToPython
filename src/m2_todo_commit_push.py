b2e31faa36858a6ba323fef3731f156d711c7189
michaelkuznicki <mkuznicki@franciscan.edu>
2024-01-22 23:57:09 -0500

    Clarified Note about Modified Files

--- src/m2_todo_commit_push.py
+++ src/m2_todo_commit_push.py
@@ -90,8 +90,9 @@ print('Hello, World')
 #
 # TODO: 5.
 #   Look at the list of files in the Project window (to the left).  Note that
-#   this file (m2_todo_and_commit_push.py) now has a 'U' next to it.
-#
+#   this file (m2_todo_and_commit_push.py) now has a 'U' or maybe an 'M' next 
+#   to it.
+#   
 #   This means that you have made changes to this file which have not yet
 #   been COMMITTED to version control and PUSHED to the cloud.
 #
